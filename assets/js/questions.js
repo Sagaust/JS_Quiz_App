1b1e6650f5ef9b97b7a80ba066216fb28a970c12
Sagaust <austinorge@yahoo.com>
2023-03-10 14:00:22 +0000

    I seperated the questions from the script file

---
+++ assets/js/questions.js
@@ -0,0 +1,94 @@
+var quizQuestions = [    {        question: "What is JavaScript?",        answers: [            "A type of coffee",            "A scripting language used to create interactive web pages",            "A new type of computer",            "A type of tree"        ],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "What is a variable?",
+        answers: [
+            "A type of insect",
+            "A container for storing data values",
+            "A type of fish",
+            "A musical instrument"
+        ],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "What is a function?",
+        answers: [
+            "A type of fruit",
+            "A reusable block of code that performs a specific task",
+            "A type of bird",
+            "A type of car"
+        ],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "What is an array?",
+        answers: [
+            "A type of pizza",
+            "A collection of data values",
+            "A type of flower",
+            "A type of building"
+        ],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "What is a loop?",
+        answers: [
+            "A type of dance",
+            "A structure used to repeat a block of code",
+            "A type of animal",
+            "A type of game"
+        ],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "What is an object?",
+        answers: [
+            "A type of machine",
+            "A collection of related data and/or functionality",
+            "A type of food",
+            "A type of book"
+        ],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "What is the DOM?",
+        answers: [
+            "A type of movie",
+            "The Document Object Model, which represents the content of a web page as objects",
+            "A type of sport",
+            "A type of building material"
+        ],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "What is a conditional statement?",
+        answers: [
+            "A type of tree",
+            "A statement that performs different actions depending on whether a condition is true or false",
+            "A type of tool",
+            "A type of dance"
+        ],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "What is the difference between '==' and '==='?",
+        answers: [
+            "'==' compares values, while '===' compares values and types",
+            "'===' compares values, while '==' compares values and types",
+            "There is no difference",
+            "Both '==' and '===' only compare types"
+        ],
+        correctAnswerIndex: 0
+    },
+    {
+        question: "What is the difference between 'let' and 'var'?",
+        answers: [
+            "'let' is block scoped, while 'var' is function scoped",
+            "'var' is block scoped, while 'let' is function scoped",
+            "There is no difference",
+            "'let' can only be used for strings, while 'var' can be used for any data type"
+        ],
+        correctAnswerIndex: 0
+    }
+];
